14574f0bc14186b1ec304094eb89c9634c0c5b27
Anneta <nikolaevaanneta@gmail.com>
2021-08-26 15:06:48 +0500

    Calculator game

---
+++ brain_games/games/calc.py
@@ -0,0 +1,24 @@
+import operator
+import random
+from typing import Tuple
+
+
+def assignment():
+    return 'What is the result of the expression?'
+
+
+def make_question_with_answer() -> Tuple[str, str]:
+    operand1 = random.randrange(1, 10)
+    operand2 = random.randrange(1, 10)
+
+    operations = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+    }
+
+    operation = random.choice(list(operations.keys()))
+
+    question = f'{operand1} {operation} {operand2}'
+    answer = str(operations[operation](operand1, operand2))
+    return str(question), answer
